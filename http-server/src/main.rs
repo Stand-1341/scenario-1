c01ccd6ac9b01910092c176853588130669045b8
Stand-1341 <hansadler687@gmail.com>
2025-09-28 17:16:52 -0500

    2.3

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -8,8 +8,10 @@ use server::Server;
 
 fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:3000".to_string();
-    let public = std::env::var("PUBLIC_PATH").unwrap_or_else(|_| "public".to_string());
-    let data = std::env::var("DATA_PATH").unwrap_or_else(|_| "data".to_string());
+    let crate_dir = env!("CARGO_MANIFEST_DIR");
+    let public = std::env::var("PUBLIC_PATH")
+        .unwrap_or_else(|_| format!("{crate_dir}/public"));
+    let data = std::env::var("DATA_PATH").unwrap_or_else(|_| format!("{crate_dir}/data"));
     let router = Router::new(public, data);
     let mut server = Server::new(addr, router);
     server.run()
